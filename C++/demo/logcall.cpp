fcbb49ad3208a3774fb0f62759f5d4813b2f460f
FluenceYHL <37389680+FluenceYHL@users.noreply.github.com>
2018-10-05 17:57:31 +0800

    Delete logcall.cpp

--- C++/demo/logcall.cpp
+++
@@ -1,9 +0,0 @@
-#include "logcall.h"
-
-inline void YHL::logCall(const char* record) {
-    std::cout << record << std::endl;
-}
-
-inline void YHL::logCerrCall(const char* record) {
-    std::cerr << record << std::endl;
-}
